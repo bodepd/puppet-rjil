98a8e8c21ea04df0db01a559eff94384f981d740
Dan Bode <bodepd@gmail.com>
2015-02-04 12:52:10 +0100

    Add multi-node deployment for cassandra
    based on a seed role.

--- manifests/cassandra.pp
+++ manifests/cassandra.pp
@@ -27,43 +27,57 @@
 # rjil::cassandra::package_name:
 #    Cassandra package name, the package name contains the major versions, so
 #    have to set the package name.
+#
+# [*seed*] If we are the seed node used for bootstrapping
+#
 
 class rjil::cassandra (
-  $seeds        = [$::ipaddress],
-  $cluster_name =  'contrail',
+  $local_ip          = $::ipaddress,
+  $seeds             = values(service_discover_consul('cassandra')),
+  $seed              = false,
+  $cluster_name      = 'contrail',
   $thread_stack_size = 300,
-  $version      = '1.2.18-1',
-  $package_name  = 'dsc12',
+  $version           = '1.2.18-1',
+  $package_name      = 'dsc12',
 ) {
+
+  $seeds_with_self = unique(concat($seeds, [$local_ip]))
+
+  if size($seeds_with_self) < 2 {
+    $fail = true
+  } else {
+    $fail = false
+  }
+
+  rjil::seed_orchestrator { 'cassandra':
+    port              => 9160,
+    check_type        => 'tcp',
+    tags              => ['real', 'contrail'],
+    seed              => $seed,
+    dep_resources     => File['/etc/cassandra/cassandra.yaml'],
+    follower_fail     => $fail,
+    follower_fail_msg => "Cassandra follower cannot be deployed without a seed, only contained ${cluster_with_self}"
+  }
 
   rjil::test { 'check_cassandra.sh': }
   # make sure that hostname is resolvable or cassandra fails
   host { 'localhost':
-    ip => '127.0.0.1',
+    ip           => '127.0.0.1',
     host_aliases => ['localhost.localdomain', $::hostname],
   }
 
   if $thread_stack_size < 229 {
     fail("JVM Thread stack size (thread_stack_size) must be > 230")
   }
+
   class {'::cassandra':
-    seeds             => $seeds,
+    listen_address    => $local_ip,
+    seeds             => $seeds_with_self,
     cluster_name      => $cluster_name,
     thread_stack_size => $thread_stack_size,
     version           => $version,
     package_name      => $package_name,
     require           => Host['localhost'],
-  }
-
-  rjil::test::check { 'cassandra':
-    type    => 'tcp',
-    address => '127.0.0.1',
-    port    => 9160,
-  }
-
-  rjil::jiocloud::consul::service { 'cassandra':
-    tags          => ['real', 'contrail'],
-    port          => 9160,
   }
 
 }
