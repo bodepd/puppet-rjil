13f321903caf179080415e952d2ec82b5fb6df5d
Dan Bode <bodepd@gmail.com>
2015-02-04 12:37:00 +0100

    Create seed_orchestrator definition
    This commit abstracts the zookeeper deployment
    pattern into a reusable defined resource type.
    
    The hope is that we can abstract our orchestration
    routines in a way that makes implementation for
    all service that require a single seed node for
    bootstrapping clusters can easily be reimplemted
    for all future usages.
    
    This patch also refactors zookeeper to use this
    definition.

--- spec/classes/zookeeper_spec.rb
+++ spec/classes/zookeeper_spec.rb
@@ -29,8 +29,8 @@ describe 'rjil::zookeeper' do
         'port' => 2181,
         'tags' => ['real', 'contrail'],
       })
-      should contain_rjil__service_blocker('leader.zookeeper').with({})
-      should contain_runtime_fail('zookeeper_list_empty').with_fail(true)
+      should contain_rjil__service_blocker('seed.zookeeper').with({})
+      should contain_runtime_fail('zookeeper_no_seed').with_fail(true)
     end
   end
 
@@ -45,31 +45,34 @@ describe 'rjil::zookeeper' do
         'id'      => 3,
         'servers' => ['server.3=foo:2888:3888']
       })
-      should contain_runtime_fail('zookeeper_list_empty').with_fail(true)
+      should contain_runtime_fail('zookeeper_no_seed').with({
+        'fail'    => true,
+        'require' => 'Rjil::Service_blocker[seed.zookeeper]'
+      })
     end
   end
 
   context 'when we find someone else from consul' do
     let :params do
       {
-        'hosts' => {'leader' => '10.1.2.4'}
+        'hosts' => {'seed' => '10.1.2.4'}
       }
     end
-    it 'should add itself with leader to cluster' do
+    it 'should add itself with seed to cluster' do
       should contain_class('zookeeper').with({
         'id'      => 3,
-        'servers' => ['server.4=leader:2888:3888', 'server.3=foo:2888:3888']
+        'servers' => ['server.4=seed:2888:3888', 'server.3=foo:2888:3888']
       })
       should_not contain_runtime_fail('zookeeper_list_empty').with_fail(true)
     end
   end
 
-  context 'when we are the leader' do
+  context 'when we are the seed' do
 
     let :params do
-      {:leader => true}
+      {:seed => true}
     end
-    it 'should configure as leader' do
+    it 'should configure as seed' do
       should contain_file('/usr/lib/jiocloud/tests/check_zookeeper.sh')
       should contain_class('zookeeper').with({
         'id'      => 3,
@@ -82,9 +85,9 @@ describe 'rjil::zookeeper' do
       })
       should contain_rjil__jiocloud__consul__service('zookeeper').with({
         'port' => 2181,
-        'tags' => ['real', 'contrail', 'leader'],
+        'tags' => ['real', 'contrail', 'seed'],
       })
-      should_not contain_rjil__service_blocker('leader.zookeeper')
+      should_not contain_rjil__service_blocker('seed.zookeeper')
       should_not contain_runtime_fail('zookeeper_list_empty')
     end
   end
