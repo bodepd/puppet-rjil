d986ac76b7fa6b668d81ca1ba173df8cab4a860f
Dan Bode <bodepd@gmail.com>
2015-02-12 14:57:36 +0100

    Split out contrail bootstrappning via seed param
    This patch splits out contrail activities that should
    only be performed on a single node to prevent race conditions.
    
    All of these operations involve creating objects against
    APIs

--- manifests/neutron/contrail.pp
+++ manifests/neutron/contrail.pp
@@ -5,7 +5,12 @@
 # [* public_subnets *]
 #   is a hash of subnetlogicalname => cidr
 #   e.g { pub_subnet1 => '100.1.0.0/16'}
-
+#
+# [*seed*]
+#   Used to specify that the current node is the seed. Only seed nodes
+#   should perform object creation actions against the api to avoid
+#   race conditions.
+#
 # NOTE: Public network will be created on services tenant. In order to specify
 # specific tenant name on which public network created, keystone.conf required
 # on neutron server which is not the case as of now.
@@ -18,6 +23,7 @@ class rjil::neutron::contrail(
   $public_rt_number     = 10000,
   $router_asn           = 64512,
   $contrail_api_server  = 'real.neutron.service.consul',
+  $seed                 = true,
 ) {
 
   include ::rjil::neutron
@@ -39,38 +45,41 @@ class rjil::neutron::contrail(
 
   include rjil::contrail::server
 
-  ##
-  # Add route target in contrail config database.
-  ##
-  contrail_rt {"default-domain:services:${public_network_name}":
-    ensure             => present,
-    rt_number          => $public_rt_number,
-    router_asn         => $router_asn,
-    api_server_address => $contrail_api_server,
-    admin_password     => $keystone_admin_password,
-    require            => Neutron_network[$public_network_name],
-  }
+  if $seed {
 
-  consul_kv{'neutron/floatingip_pool/status':
-    value   => 'ready',
-    require => Contrail_rt["default-domain:services:${public_network_name}"],
-  }
-
-  if $public_cidr {
     ##
-    # Add floating IPs
+    # Add route target in contrail config database.
     ##
-
-    neutron_network {$public_network_name:
-      ensure          => present,
-      router_external => true,
+    contrail_rt {"default-domain:services:${public_network_name}":
+      ensure             => present,
+      rt_number          => $public_rt_number,
+      router_asn         => $router_asn,
+      api_server_address => $contrail_api_server,
+      admin_password     => $keystone_admin_password,
+      require            => Neutron_network[$public_network_name],
     }
 
-    neutron_subnet {$public_subnet_name:
-      ensure       => present,
-      cidr         => $public_cidr,
-      network_name => $public_network_name,
-      before       => Contrail_rt["default-domain:services:${public_network_name}"],
+    consul_kv{'neutron/floatingip_pool/status':
+      value   => 'ready',
+      require => Contrail_rt["default-domain:services:${public_network_name}"],
+    }
+
+    if $public_cidr {
+      ##
+      # Add floating IPs
+      ##
+
+      neutron_network {$public_network_name:
+        ensure          => present,
+        router_external => true,
+      }
+
+      neutron_subnet {$public_subnet_name:
+        ensure       => present,
+        cidr         => $public_cidr,
+        network_name => $public_network_name,
+        before       => Contrail_rt["default-domain:services:${public_network_name}"],
+      }
     }
   }
 }
