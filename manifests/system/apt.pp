268113d0bf2ccb37f7a1c6d4f094ce111d2ed487
Dan Bode <bodepd@gmail.com>
2015-04-23 18:17:20 +0530

    lower priority for rustedhalo
    so that it does not conflict with kilo packages

--- manifests/system/apt.pp
+++ manifests/system/apt.pp
@@ -19,9 +19,18 @@ class rjil::system::apt (
   }
   Apt::Source<||> -> Package<||>
   Apt::Pin<||> -> Package<||>
+  File['/etc/apt/preferences.d/ignore_rusted_halo_openstack'] -> Package<||>
 
   if $enable_puppetlabs {
     include puppet::repo::puppetlabs
+  }
+
+  file { '/etc/apt/preferences.d/ignore_rusted_halo_openstack':
+    content =>
+'Package:  *
+Pin: release o=JioCloud
+Pin-Priority: 1',
+    tag   => 'package',
   }
 
   if ($proxy) {
