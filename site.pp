881fc53ccfec6d514fecee99bb41d68c457f9376
Dan Bode <bodepd@gmail.com>
2015-04-27 13:23:44 -0700

    mv keystone role to ocdb
    So that it will get the correct role specfific hiera data.
    
    Specifically, a flag needs to be set to tell it to perform
    the db_sync.

--- site.pp
+++ site.pp
@@ -101,21 +101,20 @@ node /^oc\d+/ {
 # this is a variation of the controller that has a database installed
 #
 
+#node /^ocdb\d+/ {
+#  include rjil::base
+#  include rjil::memcached
+#  include openstacklib::openstackclient
+#  include rjil::db
+#  include rjil::keystone
+#  include rjil::cinder
+#  include rjil::glance
+#  include rjil::nova::controller
+#  include rjil::openstack_zeromq
+#  include rjil::openstack_objects
+#}
+
 node /^ocdb\d+/ {
-  include rjil::base
-  include rjil::memcached
-  include openstacklib::openstackclient
-  include rjil::db
-  include rjil::keystone
-  include rjil::cinder
-  include rjil::glance
-  include rjil::nova::controller
-  include rjil::openstack_zeromq
-  include rjil::openstack_objects
-}
-
-
-node /^keystone\d+/ {
   include rjil::base
   include rjil::memcached
   include openstacklib::openstackclient
